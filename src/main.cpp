b019d9d7dc05fddcc0a956452886780c9514cf78
Max <max.royer@ens-lyon.fr>
2025-04-03 16:55:36 +0200

    petit push en leger avant le call de l'italienne

--- src/main.cpp
+++ src/main.cpp
@@ -36,16 +36,8 @@ std::unique_ptr<VertexPositionGeometry> geometryOut;
 
 typedef std::map<std::string, Vector3> BezierTile;
 
-void print_point(std::map<int, int> p) {
-  std::cout << "{";
-  for (auto c : p) {
-    std::cout << "(" << c.first << ", " << c.second << ");";
-  }
-  std::cout << "}\n";
-}
-
-bool custome_compare(const std::pair<int, int> &p1,
-                     const std::pair<int, int> &p2) {
+bool custome_compare_pair(const std::pair<int, int> &p1,
+                          const std::pair<int, int> &p2) {
   return p1.second > p2.second ||
          (p1.second == p2.second && p1.first > p2.first);
 }
@@ -59,24 +51,27 @@ transform_bary_map(std::map<int, int> bary_map) {
   return out;
 }
 
-struct Vector3Cmp {
-  bool operator()(const std::map<int, int> &lhs,
-                  const std::map<int, int> &rhs) const {
-    std::vector<std::pair<int, int>> lhs_v = transform_bary_map(lhs);
-    std::sort(lhs_v.begin(), lhs_v.end(), custome_compare);
-    std::vector<std::pair<int, int>> rhs_v = transform_bary_map(rhs);
-    std::sort(rhs_v.begin(), rhs_v.end(), custome_compare);
+struct BaryCmp {
+  bool operator()(const std::vector<std::pair<int, int>> &lhs,
+                  const std::vector<std::pair<int, int>> &rhs) const {
+    std::vector<std::pair<int, int>> lhs_sorted(3);
+    std::vector<std::pair<int, int>> rhs_sorted(3);
+    int len = sizeof(std::pair<int, int>) * 3;
+    std::partial_sort_copy(lhs.begin(), lhs.end(), lhs_sorted.begin(),
+                           lhs_sorted.end(), custome_compare_pair);
+    std::partial_sort_copy(rhs.begin(), rhs.end(), rhs_sorted.begin(),
+                           rhs_sorted.end(), custome_compare_pair);
     for (int i = 0; i < 3; i++) {
-      if (lhs_v[i].second == 0 && rhs_v[i].second == 0) {
+      if (lhs_sorted[i].second == 0 && rhs_sorted[i].second == 0) {
         return false;
       }
-      if (lhs_v[i].first < rhs_v[i].first ||
-          (lhs_v[i].first == rhs_v[i].first &&
-           lhs_v[i].second < rhs_v[i].second)) {
+      if (lhs_sorted[i].first < rhs_sorted[i].first ||
+          (lhs_sorted[i].first == rhs_sorted[i].first &&
+           lhs_sorted[i].second < rhs_sorted[i].second)) {
         return true;
       }
-      if (lhs_v[i].first != rhs_v[i].first ||
-          lhs_v[i].second != rhs_v[i].second) {
+      if (lhs_sorted[i].first != rhs_sorted[i].first ||
+          lhs_sorted[i].second != rhs_sorted[i].second) {
         return false;
       }
     }
@@ -84,7 +79,7 @@ struct Vector3Cmp {
   }
 };
 
-void print_vector_sorted(std::vector<std::pair<int, int>> v) {
+void print_bary_point(std::vector<std::pair<int, int>> v) {
   std::cout << "{";
   for (auto p : v) {
     std::cout << "(" << p.first << ";" << p.second << "),";
@@ -125,13 +120,11 @@ std::vector<Vertex> get_vertices(Face face) {
   P = shift_indices(P, argmin);
   return P;
 }
-Vector3 get_point_position(std::map<int, int> p, BezierTile B, int lod,
-                           Face f) {
-  std::vector<Vertex> v_f = get_vertices(f);
-
-  double u = (double)p[v_f[1].getIndex()] / (lod + 1);
-  double v = (double)p[v_f[2].getIndex()] / (lod + 1);
-  double w = (double)p[v_f[0].getIndex()] / (lod + 1);
+Vector3 get_point_position(std::vector<std::pair<int, int>> p, BezierTile B,
+                           int lod) {
+  double u = (double)p[1].second / (lod + 1);
+  double v = (double)p[2].second / (lod + 1);
+  double w = (double)p[0].second / (lod + 1);
   return std::pow(w, 3) * B["300"] + std::pow(u, 3) * B["030"] +
          std::pow(v, 3) * B["003"] +
          3 * (w * w * u * B["210"] + w * u * u * B["120"] +
@@ -140,12 +133,10 @@ Vector3 get_point_position(std::map<int, int> p, BezierTile B, int lod,
          6 * u * v * w * B["111"];
 }
 
-Vector3 get_normal(std::map<int, int> p, BezierTile B, int lod, Face f) {
-  std::vector<Vertex> v_f = get_vertices(f);
-
-  double u = (double)p[v_f[1].getIndex()] / (lod + 1);
-  double v = (double)p[v_f[2].getIndex()] / (lod + 1);
-  double w = (double)p[v_f[0].getIndex()] / (lod + 1);
+Vector3 get_normal(std::vector<std::pair<int, int>> p, BezierTile B, int lod) {
+  double u = (double)p[1].second / (lod + 1);
+  double v = (double)p[2].second / (lod + 1);
+  double w = (double)p[0].second / (lod + 1);
   Vector3 N = std::pow(w, 3) * B["n_200"] + std::pow(u, 3) * B["n_020"] +
               std::pow(v, 3) * B["n_002"] + u * w * B["n_110"] +
               u * v * B["n_011"] + v * w * B["n_101"];
@@ -153,44 +144,34 @@ Vector3 get_normal(std::map<int, int> p, BezierTile B, int lod, Face f) {
 }
 class Obj {
 public:
-  std::map<std::map<int, int>, int, Vector3Cmp> index_vertices;
+  std::map<std::vector<std::pair<int, int>>, int, BaryCmp> index_vertices;
   int nb_points;
   std::vector<Vector3> vertices;
   std::vector<Vector3> normals;
   std::vector<std::array<int, 3>> faces;
   Obj() { nb_points = 0; }
-  void add_vertex(std::map<int, int> v, BezierTile B, int lod, Face f) {
+  void add_vertex(std::vector<std::pair<int, int>> v, BezierTile B, int lod,
+                  Face f) {
     auto it = index_vertices.find(v);
     if (it == index_vertices.end()) {
       index_vertices.insert({v, nb_points});
-      vertices.push_back(get_point_position(v, B, lod, f));
-      normals.push_back(get_normal(v, B, lod, f));
+      vertices.push_back(get_point_position(v, B, lod));
+      normals.push_back(get_normal(v, B, lod));
       nb_points++;
     }
   }
-  int get_index_or_assign(std::map<int, int> v, BezierTile B, int lod, Face f) {
+  int get_index_or_assign(std::vector<std::pair<int, int>> v, BezierTile B,
+                          int lod, Face f) {
     auto it = index_vertices.find(v);
     if (it == index_vertices.end()) {
-      // std::cout << "Adding :";
-      // print_point(v);
-      // auto vp = transform_bary_map(v);
-      // std::sort(vp.begin(), vp.end(), custome_compare);
-      // std::cout << "It looks like this sorted :";
-      // print_vector_sorted(vp);
       add_vertex(v, B, lod, f);
       return nb_points - 1;
     } else {
-      // std::cout << "This point is already in :";
-      // print_point(v);
-      // auto vp = transform_bary_map(v);
-      // std::sort(vp.begin(), vp.end(), custome_compare);
-      // std::cout << "It looks like this sorted :";
-      // print_vector_sorted(vp);
       return it->second;
     }
   }
-  void add_face(std::array<std::map<int, int>, 3> f, BezierTile B, int lod,
-                Face face) {
+  void add_face(std::array<std::vector<std::pair<int, int>>, 3> f, BezierTile B,
+                int lod, Face face) {
     std::array<int, 3> new_f;
     for (int i = 0; i < 3; i++) {
       new_f[i] = get_index_or_assign(f[i], B, lod, face);
@@ -301,32 +282,26 @@ BezierTile get_bezier_tile(Face face) {
   return B;
 }
 
-std::vector<std::map<int, int>> get_discretized_face(int lod, Face f) {
-  // return local (barycentric position) (u,v) position
-  // of new vertices of a face
+std::vector<std::vector<std::pair<int, int>>> get_discretized_face(int lod,
+                                                                   Face f) {
   std::vector<Vertex> v_f = get_vertices(f);
-  std::vector<std::map<int, int>> positions;
+  std::vector<std::vector<std::pair<int, int>>> positions;
   for (int w = 0; w <= lod + 1; w++) {
     for (int t = 0; t <= w; t++) {
-      std::map<int, int> coord_bary;
-      coord_bary[v_f[2].getIndex()] = t;
-      coord_bary[v_f[1].getIndex()] = w - t;
-      coord_bary[v_f[0].getIndex()] = lod + 1 - w;
+      std::vector<std::pair<int, int>> coord_bary(3);
+
+      coord_bary[0] = std::make_pair(v_f[0].getIndex(), lod + 1 - w);
+      coord_bary[1] = std::make_pair(v_f[1].getIndex(), w - t);
+      coord_bary[2] = std::make_pair(v_f[2].getIndex(), t);
       positions.push_back(coord_bary);
     }
   }
-  // for (int k=0; k<positions.size(); k++) {
-  //   std::cout << positions[k] << std::endl;
-  // }
-  // std::cout << positions.size() << std::endl;
   return positions;
 }
 
 void set_pn_triangle(Face face, int lod, Obj &obj) {
-  // transform a triangle face into a curved pn triangle
-  // TODO: just inserting new point on the original face doesn't work
   BezierTile B = get_bezier_tile(face);
-  std::vector<std::map<int, int>> discretized_face =
+  std::vector<std::vector<std::pair<int, int>>> discretized_face =
       get_discretized_face(lod, face);
 
   int start_layer, first_neighbor, second_neighbor;
@@ -350,16 +325,6 @@ void set_pn_triangle(Face face, int lod, Obj &obj) {
       }
     }
   }
-
-  // for (Vector2 uv : discretized_face) {
-  //   Vector3 p = get_point_position(uv.x, uv.y, B);
-  //   auto it = seen_points.find(p);
-  //   if (it == seen_points.end()) {
-  //     seen_points.insert(p);
-  //     Vertex center_point = mesh->insertVertex(face);
-  //     geometry->vertexPositions[center_point.getIndex()] = p;
-  //   }
-  // }
 }
 
 // == MAIN
